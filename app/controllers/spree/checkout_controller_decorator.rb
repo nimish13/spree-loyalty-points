6e03cc8d93fcde9d244ffe49d4f082eae4ce9b25
nimish <nimish.gupta@vinsol.com>
2016-05-18 18:25:31 +0530

    handle case when user using existing credit card

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -5,9 +5,12 @@ module Spree
     private
 
       def sufficient_loyalty_points
-        payment_method_ids = params[:order][:payments_attributes].collect do |index, payment|
-          payment["payment_method_id"]
+        if params[:use_existing_card] == 'yes'
+          payment_method_ids = [Spree::CreditCard.find_by(id: params[:order]['existing_card']).payment_method_id]
+        else
+          payment_method_ids = params[:order][:payments_attributes].collect { |index, payment| payment["payment_method_id"] }
         end
+
         if Spree::PaymentMethod.loyalty_points_id_included?(payment_method_ids) && !@order.user.has_sufficient_loyalty_points?(@order)
           flash[:error] = Spree.t(:insufficient_loyalty_points)
           redirect_to spree.checkout_state_path(@order.state)
